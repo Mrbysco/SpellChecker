dcda8693bdf41cf8551f00d1f08aa97894b57cfd
Mrbysco <Mrbysco@users.noreply.github.com>
2022-06-25 19:38:08 +0200

    Fix the config not being registered [push]
    Also fix the default value of one of `max_suggestions`

--- Forge/src/main/java/com/mrbysco/spellchecker/SpellCheckerForge.java
+++ Forge/src/main/java/com/mrbysco/spellchecker/SpellCheckerForge.java
@@ -1,16 +1,24 @@
 package com.mrbysco.spellchecker;
 
+import com.mrbysco.spellchecker.config.SpellCheckerConfig;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.IExtensionPoint;
 import net.minecraftforge.fml.IExtensionPoint.DisplayTest;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(Constants.MOD_ID)
 public class SpellCheckerForge {
 
 	public SpellCheckerForge() {
+		IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+		ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, SpellCheckerConfig.clientSpec);
+		eventBus.register(SpellCheckerConfig.class);
+
 		//Make sure the mod being absent on the other network side does not cause the client to display the server as incompatible
 		ModLoadingContext.get().registerExtensionPoint(DisplayTest.class, () ->
 				new IExtensionPoint.DisplayTest(() -> "Trans Rights Are Human Rights", (remoteVersionString, networkBool) -> networkBool));
