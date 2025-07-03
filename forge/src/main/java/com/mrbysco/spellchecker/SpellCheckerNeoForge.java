bb5822d8cc38c01c9d88b2cbbd5c67cb93beee15
Mrbysco <Mrbysco@users.noreply.github.com>
2025-07-03 19:21:39 +0200

    Use Dist.CLIENT in the Neoforge mod annotation

--- forge/src/main/java/com/mrbysco/spellchecker/SpellCheckerNeoForge.java
+++ forge/src/main/java/com/mrbysco/spellchecker/SpellCheckerNeoForge.java
@@ -7,17 +7,13 @@ import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
 
-@Mod(Constants.MOD_ID)
+@Mod(value = Constants.MOD_ID, dist = Dist.CLIENT)
 public class SpellCheckerNeoForge {
 
 	public SpellCheckerNeoForge(IEventBus eventBus, Dist dist, ModContainer container) {
-		if (dist.isClient()) {
-			container.registerConfig(ModConfig.Type.CLIENT, SpellCheckerConfig.clientSpec);
-			eventBus.register(SpellCheckerConfig.class);
+		container.registerConfig(ModConfig.Type.CLIENT, SpellCheckerConfig.clientSpec);
+		eventBus.register(SpellCheckerConfig.class);
 
-			CommonClass.init();
-		} else {
-			Constants.LOGGER.info("SpellChecker is a client only mod, it won't do anything on the server side.");
-		}
+		CommonClass.init();
 	}
 }
